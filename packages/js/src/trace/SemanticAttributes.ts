d32c7ea3b7167e9f0fcf2e53fc15c0e1e4f7eaf3
Osher Vaknin <osherv@cisco.com>
2022-04-26 14:34:29 +0300

    added payload attributes

--- packages/js/src/trace/SemanticAttributes.ts
+++ packages/js/src/trace/SemanticAttributes.ts
@@ -20,198 +20,198 @@ export const SemanticAttributes = {
   /**
   * The request parameters.
   */
-  DB_DYNAMO_PARAMETERS: 'db.dynamo.parameters';
+  DB_DYNAMO_PARAMETERS: 'db.dynamo.parameters',
 
   /**
   * The DynamoDB response data.
   */
-  DB_DYNAMO_RESPONSE: 'db.dynamo.response';
+  DB_DYNAMO_RESPONSE: 'db.dynamo.response',
 
   /**
   * The message ID.
   */
-  AWS_SNS_MESSAGE_ID: 'aws.sns.message_id';
+  AWS_SNS_MESSAGE_ID: 'aws.sns.message_id',
 
   /**
   * The message Message structure.
   */
-  AWS_SNS_MESSAGE_STRUCTURE: 'aws.sns.message_structure';
+  AWS_SNS_MESSAGE_STRUCTURE: 'aws.sns.message_structure',
 
   /**
   * A prefix for all message attributes collection.
   */
-  AWS_SNS_MESSAGE_ATTRIBUTE: 'aws.sns.message_attribute';
+  AWS_SNS_MESSAGE_ATTRIBUTE: 'aws.sns.message_attribute',
 
   /**
   * The phone number for SMS sending.
   */
-  AWS_SNS_PHONE_NUMBER: 'aws.sns.phone_number';
+  AWS_SNS_PHONE_NUMBER: 'aws.sns.phone_number',
 
   /**
   * The topic ARN ID.
   */
-  AWS_SNS_TOPIC_ARN: 'aws.sns.topic_arn';
+  AWS_SNS_TOPIC_ARN: 'aws.sns.topic_arn',
 
   /**
   * The SNS subject.
   */
-  AWS_SNS_SUBJECT: 'aws.sns.subject';
+  AWS_SNS_SUBJECT: 'aws.sns.subject',
 
   /**
   * The SQS queue name.
   *
   * Note: Assuming this structure for QueueUrl: https://sqs.&lt;region&gt;.amazonaws.com/&lt;accountId&gt;/&lt;queueName&gt;.
   */
-  AWS_SQS_QUEUE_NAME: 'aws.sqs.queue_name';
+  AWS_SQS_QUEUE_NAME: 'aws.sqs.queue_name',
 
   /**
   * The user account id.
   *
   * Note: Assuming this structure for QueueUrl: https://sqs.&lt;region&gt;.amazonaws.com/&lt;accountId&gt;/&lt;queueName&gt;.
   */
-  AWS_SQS_ACCOUNT_ID: 'aws.sqs.account_id';
+  AWS_SQS_ACCOUNT_ID: 'aws.sqs.account_id',
 
   /**
   * The record message ID.
   *
   * Note: Captured from SendMessage,ReceiveMessage only.
   */
-  AWS_SQS_RECORD_MESSAGE_ID: 'aws.sqs.record.message_id';
+  AWS_SQS_RECORD_MESSAGE_ID: 'aws.sqs.record.message_id',
 
   /**
   * The response body.
   *
   * Note: Captured from SendMessage,ReceiveMessage only.
   */
-  AWS_SQS_RECORD_MESSAGE_BODY: 'aws.sqs.record.message_body';
+  AWS_SQS_RECORD_MESSAGE_BODY: 'aws.sqs.record.message_body',
 
   /**
   * The number in seconds to delay the message.
   */
-  AWS_SQS_RECORD_DELAY_SECONDS: 'aws.sqs.record.delay_seconds';
+  AWS_SQS_RECORD_DELAY_SECONDS: 'aws.sqs.record.delay_seconds',
 
   /**
   * A prefix for all message attributes collection.
   */
-  AWS_SQS_MESSAGE_ATTRIBUTE: 'aws.sqs.message_attribute';
+  AWS_SQS_MESSAGE_ATTRIBUTE: 'aws.sqs.message_attribute',
 
   /**
   * prefix to batch requets entries.
   */
-  AWS_SQS_REQUEST_ENTRY: 'aws.sqs.request_entry';
+  AWS_SQS_REQUEST_ENTRY: 'aws.sqs.request_entry',
 
   /**
   * prefix to batch result error entries.
   */
-  AWS_SQS_RESULT_ERROR_ENTRY: 'aws.sqs.result_error_entry';
+  AWS_SQS_RESULT_ERROR_ENTRY: 'aws.sqs.result_error_entry',
 
   /**
   * prefix to batch result entries.
   */
-  AWS_SQS_RESULT_ENTRY: 'aws.sqs.result_entry';
+  AWS_SQS_RESULT_ENTRY: 'aws.sqs.result_entry',
 
   /**
   * The number in seconds of how long this message will be available to consume.
   */
-  AWS_SQS_VISIBILITY_TIMEOUT: 'aws.sqs.visibility_timeout';
+  AWS_SQS_VISIBILITY_TIMEOUT: 'aws.sqs.visibility_timeout',
 
   /**
   * The number in seconds for the polling time.
   */
-  AWS_SQS_WAIT_TIME_SECONDS: 'aws.sqs.wait_time_seconds';
+  AWS_SQS_WAIT_TIME_SECONDS: 'aws.sqs.wait_time_seconds',
 
   /**
   * The maximum number of messages can recive.
   */
-  AWS_SQS_MAX_NUMBER_OF_MESSAGES: 'aws.sqs.max_number_of_messages';
+  AWS_SQS_MAX_NUMBER_OF_MESSAGES: 'aws.sqs.max_number_of_messages',
 
   /**
   * A prefix for all attributes names collection.
   */
-  AWS_SQS_ATTRIBUTE_NAME: 'aws.sqs.attribute_name';
+  AWS_SQS_ATTRIBUTE_NAME: 'aws.sqs.attribute_name',
 
   /**
   * A prefix for all message attributes names collection.
   */
-  AWS_SQS_MESSAGE_ATTRIBUTE_NAME: 'aws.sqs.message_attribute_name';
+  AWS_SQS_MESSAGE_ATTRIBUTE_NAME: 'aws.sqs.message_attribute_name',
 
   /**
   * prefix to record attributes.
   */
-  AWS_SQS_RECORD_ATTRIBUTE: 'aws.sqs.record.attribute';
+  AWS_SQS_RECORD_ATTRIBUTE: 'aws.sqs.record.attribute',
 
   /**
   * prefix to record message attributes.
   */
-  AWS_SQS_RECORD_MESSAGE_ATTRIBUTE: 'aws.sqs.record.message_attribute';
+  AWS_SQS_RECORD_MESSAGE_ATTRIBUTE: 'aws.sqs.record.message_attribute',
 
   /**
   * string list of all records.
   */
-  AWS_SQS_AWS_SQS_RECORD: 'aws.sqs.aws.sqs.record';
+  AWS_SQS_AWS_SQS_RECORD: 'aws.sqs.aws.sqs.record',
 
   /**
   * prefix to request metadata.
   */
-  RPC_REQUEST_METADATA: 'rpc.request.metadata';
+  RPC_REQUEST_METADATA: 'rpc.request.metadata',
 
   /**
   * The transferred body.
   */
-  RPC_REQUEST_BODY: 'rpc.request.body';
+  RPC_REQUEST_BODY: 'rpc.request.body',
 
   /**
   * prefix to response metadata.
   */
-  RPC_RESPONSE_METADATA: 'rpc.response.metadata';
+  RPC_RESPONSE_METADATA: 'rpc.response.metadata',
 
   /**
   * The transferred body.
   */
-  RPC_RESPONSE_BODY: 'rpc.response.body';
+  RPC_RESPONSE_BODY: 'rpc.response.body',
 
   /**
   * HTTP request header. This describes the prefix to HTTP headers capturing.
   */
-  HTTP_REQUEST_HEADER: 'http.request.header';
+  HTTP_REQUEST_HEADER: 'http.request.header',
 
   /**
   * HTTP response header. This describes the prefix to HTTP headers capturing.
   */
-  HTTP_RESPONSE_HEADER: 'http.response.header';
+  HTTP_RESPONSE_HEADER: 'http.response.header',
 
   /**
   * HTTP message request body.
   */
-  HTTP_REQUEST_BODY: 'http.request.body';
+  HTTP_REQUEST_BODY: 'http.request.body',
 
   /**
   * HTTP message response body.
   */
-  HTTP_RESPONSE_BODY: 'http.response.body';
+  HTTP_RESPONSE_BODY: 'http.response.body',
 
   /**
   * prefix to headers.
   */
-  MESSAGING_RABBITMQ_MESSAGE_HEADER: 'messaging.rabbitmq.message.header';
+  MESSAGING_RABBITMQ_MESSAGE_HEADER: 'messaging.rabbitmq.message.header',
 
   /**
   * The transferred payload.
   */
-  MESSAGING_RABBITMQ_PAYLOAD: 'messaging.rabbitmq.payload';
+  MESSAGING_RABBITMQ_PAYLOAD: 'messaging.rabbitmq.payload',
 
   /**
   * The transferred payload size in bytes.
   */
-  MESSAGING_RABBITMQ_PAYLOAD_SIZE: 'messaging.rabbitmq.payload_size';
+  MESSAGING_RABBITMQ_PAYLOAD_SIZE: 'messaging.rabbitmq.payload_size',
 
   /**
   * The request arguments.
   */
-  DB_REDIS_ARGUMENTS: 'db.redis.arguments';
+  DB_REDIS_ARGUMENTS: 'db.redis.arguments',
 
   /**
   * The response data.
   */
-  DB_REDIS_RESPONSE: 'db.redis.response';
+  DB_REDIS_RESPONSE: 'db.redis.response',
 }
